2a339b85bfae09272429c343098ae054b7898959
micky960 <abhrajit.sengupta@gmail.com>
2019-04-28 04:29:46 +0400

    Code for 128 bit key

--- scripts/generate_apd_orig.tcl
+++ scripts/generate_apd_orig.tcl
@@ -1,3 +1,4 @@
+## ORIGINAL DESIGN SYNTHESIS
 set design   [getenv DESIGN]
 
 set target_library { /home/projects/vlsi/libraries/65lpe/ref_lib/arm/std_cells/hvt/timing_lib/ccs/db/sc9_cmos10lpe_base_hvt_ss_nominal_max_1p08v_125c.db}
@@ -7,44 +8,9 @@ set target_library { /home/projects/vlsi/libraries/65lpe/ref_lib/arm/std_cells/h
                      /home/projects/vlsi/libraries/65lpe/ref_lib/arm/memories/sram_sp_hde/timing_lib/USERLIB_ccs_ss_1p08v_1p08v_125c.db \
                    }
 
-#read_verilog -netlist ../../Results/$design/final/${design}_final_combo.v
-analyze -library WORK -format sverilog ../../Results/$design/final/${design}_final_combo.v
-elaborate $design
-
-set key_ports [get_attribute [get_ports sfllKey_*] full_name]
-
-source ../../Results/$design/final/key.tcl
-puts $KEY
-#puts [lindex $KEY 2]
-set len [llength $KEY]
-for {set i 1} {$i <= $len} {incr i} {
-	set key_bit [lindex $KEY $i-1]
-	set_case_analysis $key_bit sfllKey_${i}_logic_${key_bit}
-	#puts [lindex $KEY $i-1]
-}
-create_clock -name VCLK -period 10  -waveform {0 5}
-#create_clock -name VCLK [get_ports CK] -period 10 -waveform {0 5}
-set input_ports  [all_inputs]
-set output_ports [all_outputs]
-set_input_delay -clock [get_clocks VCLK] -add_delay 1 [get_ports $input_ports]
-set_output_delay -clock [get_clocks VCLK] -add_delay 1 [get_ports $output_ports]
-
-
-#set_dont_touch $design
-source ../../Results/$design/final/${design}_final_combo.sdc
-puts "THE LOCKED DESIGN SYNTHESIS"
-check_design
-compile_ultra 
-report_timing
-report_area
-report_power
-
-remove_design -hier *
 puts "THE ORIGINAL DESIGN SYNTHESIS"
-analyze -library WORK -format sverilog ../../files/benchfiles/$design.v
-elaborate $design
-
-#current_design $design
+read_verilog -rtl ../../files/benchfiles/${design}.v
+current_design $design
 
 create_clock -name VCLK -period 10  -waveform {0 5}
 #create_clock -name VCLK [get_ports CK] -period 10 -waveform {0 5}
@@ -54,14 +20,15 @@ set output_ports [all_outputs]
 
 set_input_delay -clock [get_clocks VCLK] -add_delay 1 [get_ports $input_ports]
 set_output_delay -clock [get_clocks VCLK] -add_delay 1 [get_ports $output_ports]
+
 
 #set_input_delay -max 1 [get_ports $input_ports ] -clock VCLK
 #set_input_delay -min 0 [get_ports $input_ports ] -clock VCLK
-
-#set_output_delay -max 2 [get_ports $output_ports ] -clock VCLK
-#set_output_delay -min 1 [get_ports $output_ports ] -clock VCLK
-
+#
+#set_output_delay -max 1 [get_ports $output_ports ] -clock VCLK
+#set_output_delay -min 0 [get_ports $output_ports ] -clock VCLK
 check_design
+
 compile_ultra
 write -format verilog -output ../../files/benchfiles/${design}_syn.v
 read_verilog -netlist ../../files/benchfiles/${design}_syn.v
@@ -69,5 +36,4 @@ report_timing
 report_area
 report_power
 
-exit
-
+#exit
